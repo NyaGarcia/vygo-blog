e3a774a8117d329c1ee7a2343e781a577a60a612
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-10-15 21:08:18 +0200

    feat(layout): add post-layout page

---
+++ src/app/modules/posts/post-layout.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vygo-blog.layout',
+  templateUrl: './post-layout.page.html',
+})
+export class PostLayoutPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
