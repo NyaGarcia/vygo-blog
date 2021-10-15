8d9c2a6d37db3848baaa82198f6d8ee2884c8460
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-10-16 01:22:42 +0200

    feat(editor): add counter

--- src/app/modules/posts/components/editor/editor.component.ts
+++ src/app/modules/posts/components/editor/editor.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
 
+import Counter from './counter';
 import { FormGroup } from '@angular/forms';
+import Quill from 'quill';
 
+Quill.register('modules/counter', Counter);
 @Component({
   selector: 'vygo-editor',
   templateUrl: './editor.component.html',
@@ -14,6 +17,7 @@ export class EditorComponent {
       ['bold', 'italic', 'underline', 'strike'], // toggled buttons
       ['blockquote', 'code-block'],
     ],
+    counter: { container: '#counter', unit: 'word' },
   };
 
   constructor() {}
